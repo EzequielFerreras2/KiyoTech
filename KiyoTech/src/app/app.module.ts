98bfc1e1eb892f4b8c4a8bb24e3a771a775fcf1e
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2019-09-15 21:01:19 -0400

    pagin

--- KiyoTech/src/app/app.module.ts
+++ KiyoTech/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+   HomeComponent,
+   NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes                                                                                                                                                                                                                                                                                                                                                                                                                     ),
   ],
   providers: [],
   bootstrap: [AppComponent]
